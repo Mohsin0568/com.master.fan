25469a2a6b8dfdaec119b3b8f7755de6db6e885e
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-05-25 17:41:51 +0100

    initial commit, create new empty repo

---
+++ artist-info-service/src/main/java/com/master/fan/artist/ArtistInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.master.fan.artist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistInfoServiceApplication.class, args);
+	}
+
+}
---
+++ artist-info-service/src/test/java/com/master/fan/artist/ArtistInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.fan.artist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
