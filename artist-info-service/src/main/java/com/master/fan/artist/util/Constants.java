c8b0f4ca9c629fb837bf95f602e4ad439d122a38
Mohsin0568 <mohsin68.murtuza@gmail.com>
2023-05-27 13:01:08 +0100

    created file to keep constants

---
+++ artist-info-service/src/main/java/com/master/fan/artist/util/Constants.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.master.fan.artist.util;
+
+/**
+ * @author mohsin
+ * 
+ * This class will have all constants and enums
+ *
+ */
+public class Constants {
+	
+	public static final String SERVICE_UNAVAILABLE = "Service Unavailable at the moment";
+	
+	public static final String ARTIST_SERVICE_CALL = "ARTIST_SERVICE_CALL";
+	public static final String EVENTS_SERVICE_CALL = "EVENTS_SERVICE_CALL";
+	public static final String VENUES_SERVICE_CALL = "VENUES_SERVICE_CALL";
+
+}
